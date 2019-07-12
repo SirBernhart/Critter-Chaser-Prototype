1e9d96f8a0943361de021bc00cc9486ceceaa545
Bernardo Hörner Lopes <behornerl@gmail.com>
2019-07-12 01:59:40 -0300

    Direção movimento = direção em que a câmera está olhando

--- Critter Chaser/Assets/Scripts/PlayerController.cs
+++ Critter Chaser/Assets/Scripts/PlayerController.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float moveSpeed;
+
     private Rigidbody rb;
     private Transform cameraPos;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -13,13 +16,25 @@ public class PlayerController : MonoBehaviour
         cameraPos = transform.GetChild(0).GetChild(0);
     }
 
-    Vector3 forwardSpeed = new Vector3(2, 0, 0);
+    Vector3 moveDirection = Vector3.zero; 
+    Vector3 auxPoint = Vector3.zero;
     // Update is called once per frame
     void Update()
     {
+        CalculateMoveDirection();
+        Debug.Log(moveDirection);
         if (Input.GetKey(KeyCode.W))
         {
-            rb.MovePosition(transform.position + forwardSpeed);
+            rb.MovePosition(transform.position + moveDirection * moveSpeed * Time.deltaTime);
         }
+    }
+
+    // Calculates the vector3 that points from the camera to the player so as to get the direction it is looking at
+    private void CalculateMoveDirection()
+    {
+        auxPoint = transform.position;
+        auxPoint.y = cameraPos.position.y;
+        moveDirection = auxPoint - cameraPos.position;
+        moveDirection = moveDirection.normalized;
     }
 }
